7dd07b90544e33b42a30deee04d3e97dc4dd6a6e
samerbahri98 <samer.bahri@thebahrimedia.com>
2022-09-21 23:23:59 +0200

    feat: kubeconfig

--- scripts/ansible_docker_container.sh
+++ scripts/ansible_docker_container.sh
@@ -1,1 +1,6 @@
-docker run --rm -it -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -v $PWD:/ansible -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK ghcr.io/samerbahri98/k8s-admin-ansible:main /bin/bash
+docker run --rm -it\
+    -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK)\
+    -v $PWD:/ansible \
+    -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+    -e KUBECONFIG=/ansible/.kube/config \
+    ghcr.io/samerbahri98/k8s-admin-ansible:main /bin/bash
