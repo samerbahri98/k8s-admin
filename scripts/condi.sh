15f9a578d25a654407508acece0f08179baef3ff
samerbahri98 <samer.bahri@thebahrimedia.com>
2022-09-21 07:47:40 +0200

    fix: pre-commit install hadolint

--- scripts/condi.sh
+++
@@ -1,6 +0,0 @@
-for f in ./*; do
-    if [ "$f" = "./Makefile" ] || \
-        [ "$f" = "./Dockerfile" ]; then
-        echo $f;
-        fi
-    done
---
+++ scripts/prepare_precommit_env.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64 
+
+chmod +x /bin/hadolint
