ee17417cef3b5a78fcc5a4547b275dc937d34f11
samerbahri98 <samer.bahri@thebahrimedia.com>
2022-10-24 01:41:40 +0200

    fix: change s3 provider

--- terraform/modules/s3_kubeconfig/main.tf
+++ terraform/modules/s3_kubeconfig/main.tf
@@ -12,18 +12,49 @@ terraform {
   }
 }
 
-resource "b2_bucket" "kubeconfig-bucket" {
-  bucket_name = var.KUBECONFIG_BUCKET
-  bucket_type = "allPrivate"
-  default_server_side_encryption {
-    algorithm = "AES256"
+
+resource "aws_s3_bucket" "kubeconfig_bucket" {
+  bucket = var.KUBECONFIG_BUCKET
+  lifecycle {
+    prevent_destroy = true
   }
-  provider = b2
+  provider = aws
 }
 
-resource "aws_s3_object" "kubeconfig-bucket-file" {
+resource "aws_s3_bucket_acl" "kubeconfig_bucket_acl" {
+  bucket   = aws_s3_bucket.kubeconfig_bucket.id
+  acl      = "private"
+  provider = aws
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "kubeconfig_bucket_encryption" {
+  bucket = aws_s3_bucket.kubeconfig_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+  provider = aws
+}
+
+resource "aws_s3_bucket_versioning" "kubeconfig_bucket_version" {
+  bucket = aws_s3_bucket.kubeconfig_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+  provider = aws
+}
+
+resource "aws_s3_object" "kubeconfig_bucket_file" {
   bucket         = var.KUBECONFIG_BUCKET
   key            = "config"
   content_base64 = var.KUBECONFIG_B64
   provider       = aws
+  depends_on = [
+    aws_s3_bucket_acl.kubeconfig_bucket_acl,
+    aws_s3_bucket_server_side_encryption_configuration.kubeconfig_bucket_encryption,
+    aws_s3_bucket_versioning.kubeconfig_bucket_version,
+    aws_s3_bucket.kubeconfig_bucket
+  ]
 }
