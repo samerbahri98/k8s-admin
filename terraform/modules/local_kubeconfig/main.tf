10790834735a0abe2402ec48b2bb97b272968f01
samerbahri98 <samer.bahri@thebahrimedia.com>
2022-10-23 21:08:03 +0200

    fix: base64 typo

--- terraform/modules/local_kubeconfig/main.tf
+++ terraform/modules/local_kubeconfig/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 resource "local_sensitive_file" "kube_config_local" {
-  ccontent_base64 = var.KUBECONFIG_CONTENT
+  content_base64  = var.KUBECONFIG_CONTENT
   filename        = var.KUBECONFIG_PATH
   file_permission = 440
 }
