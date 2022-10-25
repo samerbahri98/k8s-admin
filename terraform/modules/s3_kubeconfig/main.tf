a582046d4f60dbefe0e7c48189cafd30aa0c9bbd
samerbahri98 <samer.bahri@thebahrimedia.com>
2022-10-25 20:20:56 +0200

    fix: s3 cors

--- terraform/modules/s3_kubeconfig/main.tf
+++ terraform/modules/s3_kubeconfig/main.tf
@@ -13,68 +13,68 @@ terraform {
 }
 
 
-resource "aws_s3_bucket" "kubeconfig_bucket" {
-  bucket = var.KUBECONFIG_BUCKET
-  lifecycle {
-    prevent_destroy = true
-  }
-  cors_rule {
-    allowed_origins = ["*"]
-    allowed_methods = [""]
-    allowed_headers = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
-  object_lock_enabled = true
-  provider            = aws
-}
+# resource "aws_s3_bucket" "kubeconfig_bucket" {
+#   bucket = var.KUBECONFIG_BUCKET
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+#   cors_rule {
+#     allowed_origins = ["*"]
+#     allowed_methods = [""]
+#     allowed_headers = ["*"]
+#     expose_headers  = ["ETag"]
+#     max_age_seconds = 3000
+#   }
+#   object_lock_enabled = true
+#   provider            = aws
+# }
 
-resource "aws_s3_bucket_acl" "kubeconfig_bucket_acl" {
-  bucket   = aws_s3_bucket.kubeconfig_bucket.id
-  acl      = "private"
-  provider = aws
-}
+# resource "aws_s3_bucket_acl" "kubeconfig_bucket_acl" {
+#   bucket   = aws_s3_bucket.kubeconfig_bucket.id
+#   acl      = "private"
+#   provider = aws
+# }
 
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "kubeconfig_bucket_encryption" {
-  bucket = aws_s3_bucket.kubeconfig_bucket.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-  provider = aws
-}
+# resource "aws_s3_bucket_server_side_encryption_configuration" "kubeconfig_bucket_encryption" {
+#   bucket = aws_s3_bucket.kubeconfig_bucket.id
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+#   provider = aws
+# }
 
-resource "aws_s3_bucket_versioning" "kubeconfig_bucket_version" {
-  bucket = aws_s3_bucket.kubeconfig_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-  provider = aws
-}
+# resource "aws_s3_bucket_versioning" "kubeconfig_bucket_version" {
+#   bucket = aws_s3_bucket.kubeconfig_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+#   provider = aws
+# }
 
-resource "aws_s3_bucket_cors_configuration" "kubeconfig_bucket_cors" {
-  bucket = aws_s3_bucket.kubeconfig_bucket.id
-  cors_rule {
-    allowed_origins = ["*"]
-    allowed_methods = [""]
-    allowed_headers = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
-  provider = aws
-}
+# resource "aws_s3_bucket_cors_configuration" "kubeconfig_bucket_cors" {
+#   bucket = aws_s3_bucket.kubeconfig_bucket.id
+#   cors_rule {
+#     allowed_origins = ["*"]
+#     allowed_methods = [""]
+#     allowed_headers = ["*"]
+#     expose_headers  = ["ETag"]
+#     max_age_seconds = 3000
+#   }
+#   provider = aws
+# }
 
 resource "aws_s3_object" "kubeconfig_bucket_file" {
   bucket         = var.KUBECONFIG_BUCKET
   key            = "config"
   content_base64 = var.KUBECONFIG_B64
   provider       = aws
-  depends_on = [
-    aws_s3_bucket_acl.kubeconfig_bucket_acl,
-    aws_s3_bucket_server_side_encryption_configuration.kubeconfig_bucket_encryption,
-    aws_s3_bucket_versioning.kubeconfig_bucket_version,
-    aws_s3_bucket_cors_configuration.kubeconfig_bucket_cors,
-  ]
+  # depends_on = [
+  #   aws_s3_bucket_acl.kubeconfig_bucket_acl,
+  #   aws_s3_bucket_server_side_encryption_configuration.kubeconfig_bucket_encryption,
+  #   aws_s3_bucket_versioning.kubeconfig_bucket_version,
+  #   aws_s3_bucket_cors_configuration.kubeconfig_bucket_cors,
+  # ]
 }
